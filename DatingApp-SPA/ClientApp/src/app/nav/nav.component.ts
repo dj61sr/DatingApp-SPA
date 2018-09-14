03eb889045532b86182d7fa7892e2266f185e029
David P Jackson Sr <dj61sr@gmail.com>
2018-09-14 15:00:23 -0500

    Added Router to the SPA application

--- DatingApp-SPA/ClientApp/src/app/nav/nav.component.ts
+++ DatingApp-SPA/ClientApp/src/app/nav/nav.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+
 import { AuthService } from '../services/auth.service';
 import { AlertifyService } from'../services/alertify.service';
+
 
 @Component({
   selector: 'app-nav',
@@ -10,18 +14,21 @@ import { AlertifyService } from'../services/alertify.service';
 export class NavComponent implements OnInit {
   model: any = {};
 
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService,
+    private router: Router) { }
 
   ngOnInit() {
   }
 
   login() {
-    this.authService.login(this.model).subscribe(() => {
+    this.authService.login(this.model).subscribe(next => {
       this.alertify.success('Logged in successfully');
       this.model.username = "";
       this.model.password = "";
     }, error => {
       this.alertify.error(error);
+      }, () => {
+      this.router.navigate(['/members']);
     });
   }
 
@@ -32,6 +39,7 @@ export class NavComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
     this.alertify.message('logged out');
+    this.router.navigate(['/home']);
   }
 
 }
