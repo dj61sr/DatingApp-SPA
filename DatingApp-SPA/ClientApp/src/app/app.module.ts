fbd848937f802f956b001789a00955aa0b99b735
David P Jackson Sr <dj61sr@gmail.com>
2018-09-25 13:41:02 -0500

    Added CanDeactivated Route Guard, @HostListener decorator, @ViewChild decorator, persisted changes to the API

--- DatingApp-SPA/ClientApp/src/app/app.module.ts
+++ DatingApp-SPA/ClientApp/src/app/app.module.ts
@@ -24,6 +24,9 @@ import { MemberDetailComponent } from './members/member-detail/member-detail.com
 import { MemberDetailResolver } from './_resolver/member-detail-resolver';
 import { MemberListResolver } from './_resolver/member-list-resolver';
 import { NgxGalleryModule } from 'ngx-gallery';
+import { MemberEditComponent } from './members/member-edit/member-edit.component';
+import { MemberEditResolver } from './_resolver/member-edit-resolver';
+import { PreventUnsavedChanges } from './_guards/prevent-unsaved-changes.guards';
 
 
 export function tokenGetter() {
@@ -41,7 +44,8 @@ export function tokenGetter() {
     ListsComponent,
     MessagesComponent,
     MemberCardComponent,
-    MemberDetailComponent
+    MemberDetailComponent,
+    MemberEditComponent
     
   ],
   imports: [
@@ -67,7 +71,9 @@ export function tokenGetter() {
     AuthGuard,
     UserService,
     MemberDetailResolver,
-    MemberListResolver
+    MemberEditResolver,
+    MemberListResolver,
+    PreventUnsavedChanges
   ],
   bootstrap: [AppComponent]
 })
